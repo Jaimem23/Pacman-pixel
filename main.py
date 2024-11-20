c22888efa38ce37c879351cae8ca13044adb25a5
Jaimem23 <140969227+Jaimem23@users.noreply.github.com>
2024-11-20 12:36:38 +0100

    Check the input for Pacman in main class
    Add pyxel update with checks the input and changes the direction of pacman

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
 from colision_handler import ColisionHandler
 from UI_handler import UIHandler
+import pyxel
+from pacman import Pacman
+
+class main():
+    def __init__(self) -> None:
+        pyxel.init(120,160)
+        #Create the pacman
+        self.pacman = Pacman(0,0,16,16,"pacman")
+
+        pyxel.run(self.update,self.draw)
+
+    def update(self):
+
+        #Check the input of the user
+
+        if(pyxel.btn(pyxel.KEY_W) or pyxel.btn(pyxel.KEY_UP)):
+            self.pacman.direction = "up"
+        elif(pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_LEFT)):
+            self.pacman.direction = "left"
+        elif(pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.KEY_RIGHT)):
+            self.pacman.direction = "right"
+        elif(pyxel.btn(pyxel.KEY_S) or pyxel.btn(pyxel.KEY_DOWN)):
+            self.pacman.direction = "down"
+
+        if(pyxel.btn(pyxel.KEY_ESCAPE)):
+            pyxel.quit()
+
+main()
--- pacman.py
+++ pacman.py
@@ -1,9 +1,8 @@
 """Create classes to represent the objects needed for the final project. For all of them, create init
 methods, properties and setters."""
 from sprite import Sprite
-
 class Pacman(Sprite):
-    def __init__(self, x_pos, y_pos, widht, height, image,direction):
+    def __init__(self, x_pos, y_pos, widht, height, image,direction:str = "right"):
         super().__init__(x_pos, y_pos, widht, height, image)
         self.direction = direction
     
