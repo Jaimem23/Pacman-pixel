85fe18a370d3455e52a68fead67681579aaa6fe7
Jaimem23 <140969227+Jaimem23@users.noreply.github.com>
2024-11-26 09:17:23 +0100

    Change the name from blinky_obj to blinky

--- blinky.py
+++ blinky.py
@@ -4,4 +4,4 @@ class Blinky(Ghost):
     def __init__(self, x_pos, y_pos, widht, height, x_pos_tile,y_pos_tile,direction):
         super().__init__(x_pos, y_pos, widht, height,x_pos_tile,y_pos_tile,direction)
 
-blinky = Blinky(100,100,16,16,0,BLINKY_Y_TILE,"up")
+blinky = Blinky(100,100,16,16,0,BLINKY_Y_TILE,"right")
