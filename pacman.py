e31fb3033df0388771e61c568c56b12a9ee3564a
Jaimem23 <140969227+Jaimem23@users.noreply.github.com>
2024-11-30 14:59:13 +0100

    Create __can_move and add next_direction attribute

--- pacman.py
+++ pacman.py
@@ -14,48 +14,44 @@ class Pacman(Sprite):
         self.__animation_timer = 5
         self.__next_direction = self.direction
         
-    def move_prov(self):
+    def change_direction(self):
         """A function that cheks the direction of the pacman based on the input"""
+        #Only change to that direction if he can move
+        if self.__can_move(self.__next_direction):
+            self.direction = self.__next_direction
+            if self.direction == "right":
+                self.y_pos_tile = PACMAN_RIGHT_TILE_Y
+            elif self.direction == "left":
+                self.y_pos_tile = PACMAN_LEFT_TILE_Y
+            elif self.direction == "up":
+                self.y_pos_tile = PACMAN_UP_TILE_Y
+            elif self.direction == "down":
+                #Change the direction of the sprite of the pacman
+                self.y_pos_tile = PACMAN_DOWN_TILE_Y
+
         if(btn(KEY_W) or btn(KEY_UP)):
             #Change the direction the pacman wants to go
             self.__next_direction = "up"
-            #Only change to that direction if he can move
-            if self.__can_move(self.__next_direction):
-                self.direction = self.__next_direction
-                #Change the direction of the sprite of the pacman
-                self.y_pos_tile = PACMAN_UP_TILE_Y
 
         elif(btn(KEY_A) or btn(KEY_LEFT)):
             #Change the direction the pacman wants to go
             self.__next_direction = "left"
-            #Only change to that direction if he can move
-            if self.__can_move(self.__next_direction):
-                self.direction = self.__next_direction
-                #Change the direction of the sprite of the pacman
-                self.y_pos_tile = PACMAN_LEFT_TILE_Y
 
         elif(btn(KEY_D) or btn(KEY_RIGHT)):
             #Change the direction the pacman wants to go
-            self.__next_direction = "left"
-            #Only change to that direction if he can move
-            if self.__can_move(self.__next_direction):
-                self.direction = self.__next_direction
-                #Change the direction of the sprite of the pacman
-                self.y_pos_tile = PACMAN_UP_TILE_Y
+            self.__next_direction = "right"
 
         elif(btn(KEY_S) or btn(KEY_DOWN)):
             #Change the direction the pacman wants to go
             self.__next_direction = "down"
-            #Only change to that direction if he can move
-            if self.__can_move(self.__next_direction):
-                self.direction = self.__next_direction
-                #Change the direction of the sprite of the pacman
-                self.y_pos_tile = PACMAN_DOWN_TILE_Y
+            
 
+
+    
 
     def move(self):
         """A function that moves pacman with his direction"""
-        if self.direction == "right"  and ((self.map_matrix[int((self.y_pos/8) + 1)][int((self.x_pos/8) + 3)] == 0) and (self.map_matrix[int(self.y_pos/8)][int((self.x_pos/8) + 3)] == 0)):
+        if self.direction == "right"  and self.__can_move(self.direction):
             #Allow pacman to go from right to left
             if(self.x_pos > SCREEN_WIDTH):  
                 #Make the pacman appear on the other side of the screen exactly the coordinates of its size
@@ -70,7 +66,7 @@ class Pacman(Sprite):
                     self.x_pos_tile += 16
                 else: 
                     self.x_pos_tile = 0
-        elif self.direction == "left" and ((self.map_matrix[int((self.y_pos/8) + 1)][int((self.x_pos/8) - 1)] == 0) and (self.map_matrix[int(self.y_pos/8)][int((self.x_pos/8) - 1)] == 0)):
+        elif self.direction == "left" and self.__can_move(self.direction):
             #Allow pacman to go from left to right
             if(self.x_pos < -16):
                 self.x_pos = SCREEN_WIDTH
@@ -84,7 +80,7 @@ class Pacman(Sprite):
                     self.x_pos_tile += 16
                 else: 
                     self.x_pos_tile = 0
-        elif self.direction == "up" and ((self.map_matrix[int((self.y_pos/8) - 1)][int(self.x_pos/8)] == 0) and (self.map_matrix[int((self.y_pos/8) - 1)][int((self.x_pos/8) + 1)] == 0)):
+        elif self.direction == "up" and self.__can_move(self.direction):
             #Need to substract one since the left corner is the origin
             self.y_pos -= 1 * self.velocity 
             #Logic to make the animation of pacman moving the mouth
@@ -95,94 +91,7 @@ class Pacman(Sprite):
                     self.x_pos_tile += 16
                 else: 
                     self.x_pos_tile = 0
-        elif self.direction == "down" and ((self.map_matrix[int((self.y_pos/8) + 3)][int(self.x_pos/8)] == 0) and (self.map_matrix[int((self.y_pos/8) + 3)][int((self.x_pos/8) + 1)] == 0)):
-            self.y_pos += 1 * self.velocity
-
-            #Logic to make the animation of pacman moving the mouth
-            # Only update every N frames
-            if self.__animation_timer == 0:
-                #If is not the last sprite, move to the next one  
-                if self.x_pos_tile != 32:
-                    self.x_pos_tile += 16
-                else: 
-                    self.x_pos_tile = 0
-        
-        # Increment animation timer, reset periodically
-        self.__animation_timer = (self.__animation_timer + 1) % self.__animation_speed
-
-    def change_direction(self):
-        """A function that cheks the direction of the pacman based on the input"""
-        if(btn(KEY_W) or btn(KEY_UP)):
-            #Change the direction the pacman wants to go
-            self.direction = "up"
-            #Change the direction of the sprite of the pacman
-            self.y_pos_tile = PACMAN_UP_TILE_Y
-        elif(btn(KEY_A) or btn(KEY_LEFT)):
-            self.direction = "left"
-            self.y_pos_tile = PACMAN_LEFT_TILE_Y
-        elif(btn(KEY_D) or btn(KEY_RIGHT)):
-            self.direction = "right"
-            self.y_pos_tile = PACMAN_RIGHT_TILE_Y
-        elif(btn(KEY_S) or btn(KEY_DOWN)):
-            self.direction = "down"
-            self.y_pos_tile = PACMAN_DOWN_TILE_Y
-
-    def move(self):
-        """A function that moves pacman with his direction"""
-        if self.direction == "right"  and \
-            ((self.map_matrix[int(self.y_pos/8)][int((self.x_pos/8) + 4)] == 0) and \
-            (self.map_matrix[int(self.y_pos/8) + 1][int((self.x_pos/8) + 4)] == 0)) and \
-            (self.map_matrix[int(self.y_pos/8) +2][int((self.x_pos/8) + 4)] == 0) and \
-            (self.map_matrix[int(self.y_pos/8) +3][int((self.x_pos/8) + 4)] == 0):
-            #Allow pacman to go from right to left
-            if(self.x_pos > SCREEN_WIDTH):  
-                #Make the pacman appear on the other side of the screen exactly the coordinates of its size
-                #That way it gets teleported without the player noticing the change of coordinates            
-                self.x_pos = -self.width
-            self.x_pos += 1 * self.velocity
-            #Logic to make the animation of pacman moving the mouth
-            #Only update every N frames
-            if self.__animation_timer == 0:
-                #If is not the last sprite, move to the next one  
-                if self.x_pos_tile != 32:
-                    self.x_pos_tile += 16
-                else: 
-                    self.x_pos_tile = 0
-        elif self.direction == "left" and ((self.map_matrix[int(self.y_pos/8) + 1][int((self.x_pos/8) - 1)] == 0) and \
-            (self.map_matrix[int(self.y_pos/8)][int((self.x_pos/8) -1 )] == 0)) and \
-            (self.map_matrix[int(self.y_pos/8) +2][int((self.x_pos/8) - 1)] == 0) and \
-            (self.map_matrix[int(self.y_pos/8) +3][int((self.x_pos/8) - 1)] == 0):
-            #Allow pacman to go from left to right
-            if(self.x_pos < -16):
-                self.x_pos = SCREEN_WIDTH
-            self.x_pos -= 1 * self.velocity
-
-            #Logic to make the animation of pacman moving the mouth
-            # Only update every N frames
-            if self.__animation_timer == 0:
-                #If is not the last sprite, move to the next one  
-                if self.x_pos_tile != 32:
-                    self.x_pos_tile += 16
-                else: 
-                    self.x_pos_tile = 0
-        elif self.direction == "up" and ((self.map_matrix[int(self.y_pos/8) -1][int((self.x_pos/8))] == 0) and \
-            (self.map_matrix[int(self.y_pos/8)-1][int((self.x_pos/8) + 1)] == 0)) and \
-            (self.map_matrix[int(self.y_pos/8)-1][int((self.x_pos/8) + 2)] == 0) and \
-            (self.map_matrix[int(self.y_pos/8)-1][int((self.x_pos/8) + 3)] == 0):
-            #Need to substract one since the left corner is the origin
-            self.y_pos -= 1 * self.velocity 
-            #Logic to make the animation of pacman moving the mouth
-            # Only update every N frames
-            if self.__animation_timer == 0:
-                #If is not the last sprite, move to the next one  
-                if self.x_pos_tile != 32:
-                    self.x_pos_tile += 16
-                else: 
-                    self.x_pos_tile = 0
-        elif self.direction == "down" and ((self.map_matrix[int(self.y_pos/8) +4][int((self.x_pos/8))] == 0) and \
-            (self.map_matrix[int(self.y_pos/8)+4][int((self.x_pos/8) + 1)] == 0)) and \
-            (self.map_matrix[int(self.y_pos/8)+4][int((self.x_pos/8) + 2)] == 0) and \
-            (self.map_matrix[int(self.y_pos/8)+4][int((self.x_pos/8) + 3)] == 0):
+        elif self.direction == "down" and self.__can_move(self.direction):
             self.y_pos += 1 * self.velocity
 
             #Logic to make the animation of pacman moving the mouth
@@ -211,8 +120,18 @@ class Pacman(Sprite):
             self.__velocity = velocity
 
     def __can_move(self,direction):
-        #Logic to check if it can move
-        pass
+        #Check the four tiles the pacman occupies
+        for tile in range(4):
+            if direction == "right" and self.map_matrix[int(self.y_pos/8) + tile][int((self.x_pos/8) + 4)] != 0:
+                #If a tile is a wall, return False
+                return False
+            elif direction == "left" and self.map_matrix[int(self.y_pos/8) + tile][int((self.x_pos/8) - 1)] != 0:
+                return False
+            elif direction == "up" and self.map_matrix[int(self.y_pos/8) -1][int((self.x_pos/8)) + tile] != 0:
+                return False
+            elif direction == "down" and self.map_matrix[int(self.y_pos/8) +4][int((self.x_pos/8)) + tile] != 0:
+                return False
+        return True
 
     #read only properties
     @property
@@ -259,6 +178,8 @@ class Pacman(Sprite):
             raise TypeError("The map matrix should be a list")
         else:
             self.__map_matrix = map_matrix
+    
+
 
 #Create the pacman
 pacman = Pacman(PACMAN_INITIAL_X, PACMAN_INITIAL_Y, 8, 8, 0, 0, 2, maze.map_matrix)
